ddb8036434e90f45c8fa8cd59572426c3bd3389d
Tanay <tanaydwivedi456@gmail.com>
2022-04-02 13:56:14 +0530

    fix:placed return statement in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-      <Route path="/signin" element={<Signin/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-    </Routes>
-  </Router>
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
