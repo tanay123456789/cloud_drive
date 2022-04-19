a85f623bfa069de9a20dd56724939aaaefb3e4d6
tanay1234567894 <tanaydwivedi456@gmail.com>
2022-04-19 16:43:53 +0530

    feat:added firebase config

---
+++ drive/src/firebase/index.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJ_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  };
+
+export const auth=getAuth();
+export const app=initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const provider=new GoogleAuthProvider();
--- drive/src/pages/Signin.js
+++ drive/src/pages/Signin.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 
 const Signin = () => {
     return <h1>signin page</h1>;
