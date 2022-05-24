092912be2430e332b2656ddcc19afa0578452c10
Tanay <tanaydwivedi456@gmail.com>
2022-05-24 20:48:48 +0530

    style:styled signin button

--- drive/src/pages/signin/index.js
+++ drive/src/pages/signin/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import GoogleButton from 'react-google-button'
+import "./styles.css"
 
 const Signin=()=>{
   return(
-    <div>
+    <div className='auth-btn'>
       <GoogleButton 
       onClick={() => { 
         console.log('Google button clicked')
